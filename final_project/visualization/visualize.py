861624a39743ab68ccbb63152c82c9a8bd013c34
afyepesb87 <afyb87@gmail.com>
2023-10-24 09:19:04 -0500

    first commit

---
+++ final_project/visualization/visualize.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+
+def covid_time_series(df: pd.DataFrame):
+    print("kakakakaka")
+    sns.lineplot(
+        data=df,
+        x="date",
+        y="value",
+        hue="country_region"
+    )
+
+    plt.xticks(rotation=15)
+    plt.xlabel("Date")
+    plt.ylabel("Value")
+    plt.title("Latam covid time series")
+    plt.show();
